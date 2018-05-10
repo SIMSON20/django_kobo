6a34c0e33800954e7825de439783f507d293788e
Thomas.Maschler <tmaschler@wri.org>
2018-05-10 23:03:26 +0300

    Use django-import-export

--- kobo_project/kobo/admin.py
+++ kobo_project/kobo/admin.py
@@ -4,6 +4,9 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 from datetime import datetime
+from import_export import resources
+from import_export.fields import Field
+import tablib
 
 
 class ConnectionAdmin(admin.ModelAdmin):
@@ -12,28 +15,20 @@ class ConnectionAdmin(admin.ModelAdmin):
 
     def sync(self, request, queryset):
 
-        #onnections = queryset.values()
         for connection in queryset:
 
-            # delete all records for selected connection
-            KoboData.objects.filter(auth_user=connection.auth_user).delete()
+            dataset = tablib.dict(self.get_kobo_forms(connection))
 
-            # fetch all forms
-            forms = self.get_kobo_forms(connection)
+            kobodata_resource = resources.modelresource_factory(model=KoboData)
+            kobodata_resource.connection = connection
+            result = kobodata_resource.import_data(dataset, dry_run=True)
+            if not result.has_errors():
+                result = kobodata_resource.import_data(dataset, dry_run=False)
+            else:
+                pass
 
-            # fetch all asssets and data for selected forms and write to table
-            for form in forms:
-                KoboData.objects.create(
-                                   auth_user = connection,
-                                   dataset_id= int(form['formid']),
-                                   dataset_uuid=form['id_string'],
-                                   dataset_owner = form['owner'][40:],
-                                   tags = self.get_kobo_assets(connection, form['id_string'])["tag_string"].split(","),
-                                   dataset_name = form['title'],
-                                   dataset_year = int(form['date_created'][0:4]),
-                                   last_submission_time = datetime.strptime(form['last_submission_time'], '%Y-%m-%dT%H:%M:%S.%fZ'),
-                                   dataset = self.get_kobo_data(connection, form["formid"])
-                                   )
+            connection.last_update_time = datetime.now()
+            connection.save()
 
         self.message_user(request, "Data successfully updated")
 
@@ -43,17 +38,6 @@ class ConnectionAdmin(admin.ModelAdmin):
         auth_user = connection.auth_user.strip()
         auth_passwd = connection.auth_pass.strip()
         url = "{}/{}?format=json".format(host, "forms")
-        r = requests.get(url, auth=HTTPBasicAuth(auth_user, auth_passwd))
-        return json.loads(r.text)
-
-    @staticmethod
-    def get_kobo_assets(connection, dataset_uuid):
-
-        host = connection.host_assets.strip()
-        auth_user = connection.auth_user.strip()
-        auth_passwd = connection.auth_pass.strip()
-        url = "{}{}/?format=json".format(host, dataset_uuid)
-
         r = requests.get(url, auth=HTTPBasicAuth(auth_user, auth_passwd))
         return json.loads(r.text)
 
@@ -71,8 +55,63 @@ class ConnectionAdmin(admin.ModelAdmin):
 
 
 class KoboDataAdmin(admin.ModelAdmin):
-    list_display = ['dataset_name', 'dataset_year', 'tags', 'dataset_owner', 'last_submission_time']
+    list_display = ['dataset_name', 'dataset_year', 'tags', 'dataset_owner', 'last_submission_time', 'last_update_time']
     ordering = ['dataset_owner', 'dataset_name', 'dataset_year']
+
+
+class KoboDataResource(resources.ModelResource):
+
+    def __init__(self):
+        self.start_time = datetime.now()
+        self.connection = None
+        self.dataset_id = Field(column_name='formid')
+        self.dataset_uuid = Field(column_name='id_string')
+        self.dataset_name = Field(column_name='title')
+        self.dataset_year = Field(column_name='date_created')
+        self.last_submission_time = Field(column_name='last_submission_time')
+
+    @staticmethod
+    def get_kobo_assets(connection, dataset_uuid):
+        host = connection.host_assets.strip()
+        auth_user = connection.auth_user.strip()
+        auth_passwd = connection.auth_pass.strip()
+        url = "{}{}/?format=json".format(host, dataset_uuid)
+        r = requests.get(url, auth=HTTPBasicAuth(auth_user, auth_passwd))
+        return json.loads(r.text)
+
+    def dehydrate_auth_user(self, kobodata):
+        return self.connection.auth_user
+
+    @staticmethod
+    def dehydrate_dataset_owner(kobodata):
+        return kobodata.owner[40:].split('?')[0]
+
+    @staticmethod
+    def dehydrate_dataset_year(kobodata):
+        return kobodata.date_created[0:4]
+
+    @staticmethod
+    def dehydrate_last_submission_time(kobodata):
+        if kobodata.last_submission_time is not None:
+           return datetime.strptime(kobodata.last_submission_time, '%Y-%m-%dT%H:%M:%S.%fZ')
+        else:
+            return None
+
+    @staticmethod
+    def dehydrate_last_update_time(kobodata):
+        return datetime.now()
+
+    def dehydrate_tags(self, kobodata):
+        return self.get_kobo_assets(self.connection, kobodata.id_string)["tag_string"].split(",")
+
+    def after_save_instance(self, instance, using_transactions, dry_run):
+        queryset = self.get_queryset()
+        q = queryset.objects.filter(last_update_time__smallerthan=self.start_time)
+        q.delete()
+
+    class Meta:
+        model = KoboData
+
 
 admin.site.register(Connection, ConnectionAdmin)
 admin.site.register(KoboData, KoboDataAdmin)
--- kobo_project/kobo/models.py
+++ kobo_project/kobo/models.py
@@ -8,6 +8,7 @@ class Connection(models.Model):
     auth_pass = models.CharField(max_length=200)
     host_assets = models.CharField(max_length=200)
     host_api = models.CharField(max_length=200)
+    last_update_time = models.DateTimeField(null=True)
 
     def __str__(self):
         return "{}@{}".format(self.auth_user, self.host_api)
@@ -25,7 +26,18 @@ class KoboData(models.Model):
     dataset_name = models.TextField(null=True)
     dataset_year = models.IntegerField(null=True)
     last_submission_time = models.DateTimeField(null=True)
-    dataset = JSONField(null=True)
+    last_update_time = models.DateTimeField(null=True)
+    #dataset = JSONField(null=True)
 
     def __str__(self):
-        return "{}@{}".format(self.auth_user, self.dataset_id)
+        return "{}, {}, {}".format(self.dataset_owner, self.dataset_name, self.dataset_year)
+
+    def is_uptodate(self):
+        if self.last_submission_time is None:
+            return "No submissions"
+        elif self.last_update_time is None:
+            return "Never updated"
+        elif self.last_update_time >= self.last_submission_time:
+            return "Up to date"
+        else:
+            return "Out of date"
