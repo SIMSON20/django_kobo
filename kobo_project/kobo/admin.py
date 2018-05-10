6ef70bf12b7d0950f8d994ec65b32ebf46d166ac
Thomas.Maschler <tmaschler@wri.org>
2018-05-10 23:50:31 +0300

    move sync method to KoboDataAdmin class

--- kobo_project/kobo/admin.py
+++ kobo_project/kobo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Connection, KoboData
 from .resources import KoboDataResource
 import requests
@@ -8,23 +9,33 @@ from datetime import datetime
 import tablib
 
 
+@admin.register(Connection)
 class ConnectionAdmin(admin.ModelAdmin):
+    """
+    Admin class for Connections
+    """
     list_display = ['auth_user', 'host_assets']
     ordering = ['auth_user']
 
     def sync(self, request, queryset):
+        """
+        Add action to fetch latest data from Kobo
+        :param request:
+        :param queryset:
+        :return:
+        """
 
         for connection in queryset:
 
             dataset = tablib.dict(self.get_kobo_forms(connection))
 
-            kobodata_resource = KoboDataResource()
-            kobodata_resource.connection = connection
-            result = kobodata_resource.import_data(dataset, dry_run=True)
-            if not result.has_errors():
-                result = kobodata_resource.import_data(dataset, dry_run=False)
-            else:
-                pass
+            # kobodata_resource = KoboDataResource()
+            # kobodata_resource.connection = connection
+            # result = kobodata_resource.import_data(dataset, dry_run=True)
+            # if not result.has_errors():
+            #     result = kobodata_resource.import_data(dataset, dry_run=False)
+            # else:
+            #     pass
 
             connection.last_update_time = datetime.now()
             connection.save()
@@ -53,10 +64,41 @@ class ConnectionAdmin(admin.ModelAdmin):
     sync.short_description = "Sync data"
 
 
-class KoboDataAdmin(admin.ModelAdmin):
+@admin.register(KoboData)
+class KoboDataAdmin(ImportExportModelAdmin):
     list_display = ['dataset_name', 'dataset_year', 'tags', 'dataset_owner', 'last_submission_time', 'last_update_time']
     ordering = ['dataset_owner', 'dataset_name', 'dataset_year']
 
+    def sync(self, request, queryset):
+        """
+        Add action to fetch latest data from Kobo
+        :param request:
+        :param queryset:
+        :return:
+        """
 
-admin.site.register(Connection, ConnectionAdmin)
-admin.site.register(KoboData, KoboDataAdmin)
+        for q in queryset:
+
+            connection = q.connection
+
+            dataset = tablib.dict(self.get_kobo_form(connection, q.dataset_id))
+
+            kobodata_resource = KoboDataResource()
+            kobodata_resource.connection = connection
+            result = kobodata_resource.import_data(dataset, dry_run=True)
+            if not result.has_errors():
+                result = kobodata_resource.import_data(dataset, dry_run=False)
+            else:
+                pass
+
+    @staticmethod
+    def get_kobo_form(connection, form_uuid):
+        host = connection.host_api.strip()
+        auth_user = connection.auth_user.strip()
+        auth_passwd = connection.auth_pass.strip()
+        url = "{}/{}/{}?format=json".format(host, "forms", form_uuid)
+        r = requests.get(url, auth=HTTPBasicAuth(auth_user, auth_passwd))
+        return json.loads(r.text)
+
+    actions = [sync]
+    sync.short_description = "Sync data"
--- kobo_project/kobo/resources.py
+++ kobo_project/kobo/resources.py
@@ -8,8 +8,18 @@ import json
 
 
 class KoboDataResource(resources.ModelResource):
+    """
+    Resource for import Kobo Data directly from the Kobo API
+    Existing data will be overwritten with new data
+    Data no longer available on Kobo will be delete after import
+    """
 
     def __init__(self):
+        """
+        Map fields from Kobo API to table
+        Set start time to find out later which data were updated
+        Set connection
+        """
         self.start_time = datetime.now()
         self.connection = None
         self.dataset_id = Field(column_name='formid')
@@ -20,6 +30,12 @@ class KoboDataResource(resources.ModelResource):
 
     @staticmethod
     def get_kobo_assets(connection, dataset_uuid):
+        """
+        Connect to Kobo Assets API and fetch data for selected form
+        :param connection:
+        :param dataset_uuid:
+        :return:
+        """
         host = connection.host_assets.strip()
         auth_user = connection.auth_user.strip()
         auth_passwd = connection.auth_pass.strip()
@@ -28,18 +44,38 @@ class KoboDataResource(resources.ModelResource):
         return json.loads(r.text)
 
     def dehydrate_auth_user(self, kobodata):
+        """
+        Get user name from connection
+        :param kobodata:
+        :return:
+        """
         return self.connection.auth_user
 
     @staticmethod
     def dehydrate_dataset_owner(kobodata):
+        """
+        Extract dataset owner from kobo hyperlink
+        :param kobodata:
+        :return:
+        """
         return kobodata.owner[40:].split('?')[0]
 
     @staticmethod
     def dehydrate_dataset_year(kobodata):
+        """
+        Extract year from created date timestamp
+        :param kobodata:
+        :return:
+        """
         return kobodata.date_created[0:4]
 
     @staticmethod
     def dehydrate_last_submission_time(kobodata):
+        """
+        Convert last submission time to datetime object
+        :param kobodata:
+        :return:
+        """
         if kobodata.last_submission_time is not None:
            return datetime.strptime(kobodata.last_submission_time, '%Y-%m-%dT%H:%M:%S.%fZ')
         else:
@@ -47,12 +83,29 @@ class KoboDataResource(resources.ModelResource):
 
     @staticmethod
     def dehydrate_last_update_time(kobodata):
+        """
+        Set last update time to now
+        :param kobodata:
+        :return:
+        """
         return datetime.now()
 
     def dehydrate_tags(self, kobodata):
+        """
+        Fetch tags from Kobo form
+        :param kobodata:
+        :return:
+        """
         return self.get_kobo_assets(self.connection, kobodata.id_string)["tag_string"].split(",")
 
     def after_save_instance(self, instance, using_transactions, dry_run):
+        """
+        Delete not updated data after instance was saved
+        :param instance:
+        :param using_transactions:
+        :param dry_run:
+        :return:
+        """
         if not dry_run:
             queryset = self.get_queryset()
             q = queryset.objects.filter(last_update_time__smallerthan=self.start_time)
