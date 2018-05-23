a12c647909f4309e85f9863ad249c8883c37faf2
Thomas.Maschler <tmaschler@wri.org>
2018-05-23 10:38:18 -0400

    rearrange code in private methods

--- kobo_project/kobo/admin.py
+++ kobo_project/kobo/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+# from django.contrib.messages import constants as messages
 from import_export.admin import ImportExportModelAdmin
 from .models import Connection, KoboData
 from .resources import KoboDataFromFileResource, KoboDataFromKoboResource
@@ -35,7 +36,7 @@ class ConnectionAdmin(admin.ModelAdmin):
             kobodata_resource.connection = connection
             result = kobodata_resource.import_data(dataset, raise_errors=True, dry_run=True)
             if not result.has_errors():
-                result = kobodata_resource.import_data(dataset, dry_run=False)
+                kobodata_resource.import_data(dataset, dry_run=False)
             else:
                 raise forms.ValidationError("Import failed!")
 
@@ -72,39 +73,70 @@ class KoboDataAdmin(ImportExportModelAdmin):
         """
 
         for form in queryset:
-            #import pdb; pdb.set_trace()
             dataset = self.get_kobo_data(form.auth_user, form.dataset_id)
 
-            # make sure every row has the same keys.
-            # get all unique keys
-            keys = set()
-            for row in dataset:
-                keys |= set(row.keys())
+            if "bns" in form.tags:
+                self._sync_bns(dataset, form, request)
 
-            # add missing keys to rows
-            for row in dataset:
-                key_set = set(keys)
-                key_set -= set(row.keys())
-                for key in key_set:
-                    row[key] = None
+    def _sync_bns(self, dataset, form, request):
+        """
+        Syncs selected BNS data with Kobo
+        :param dataset:
+        :param form:
+        :param request:
+        :return:
+        """
+        dataset = self._normalize_data(dataset)
 
-            dataset = tablib.Dataset().load(json.dumps(dataset, sort_keys=True))
+        answer_resource = AnswerFromKoboResource()
+        answer_resource.form = form
 
-            answer_resource = AnswerFromKoboResource()
-            answer_resource.form = form
+        answer_gps_resource = AnswerGPSFromKoboResource()
 
-            answer_gps_resource = AnswerGPSFromKoboResource()
+        result = answer_resource.import_data(dataset, raise_errors=True, dry_run=True)
+        if not result.has_errors():
+            answer_resource.import_data(dataset, dry_run=False)
+            self.message_user(request,
+                              "Successfully updated {} answers for form {}".format(len(dataset), form.dataset_name))
 
-            result = answer_resource.import_data(dataset, raise_errors=True, dry_run=True)
+            result = answer_gps_resource.import_data(dataset, raise_errors=True, dry_run=True)
             if not result.has_errors():
-                answer_resource.import_data(dataset, dry_run=False)
-                result = answer_gps_resource.import_data(dataset, raise_errors=True, dry_run=True)
-                if not result.has_errors():
-                    answer_gps_resource.import_data(dataset, dry_run=False)
-                else:
-                    raise forms.ValidationError("Import of AnswersGPS failed!")
+                answer_gps_resource.import_data(dataset, dry_run=False)
+                self.message_user(request, "Successfully updated {} GPS coordinates for form {}".format(len(dataset),
+                                                                                                        form.dataset_name))
             else:
-                raise forms.ValidationError("Import of Answers failed!")
+                self.message_user(request, "Failed to updated GPS coordinates for form {}".format(form.dataset_name),
+                                  level=messages.ERROR)
+
+                # raise forms.ValidationError("Import of AnswersGPS failed!")
+        else:
+            self.message_user(request, "Failed to updated answers for form {}".format(form.dataset_name),
+                              level=messages.ERROR)
+            # raise forms.ValidationError("Import of Answers failed!")
+
+    @staticmethod
+    def _normalize_data(dataset):
+        """
+        Expects a list of dictionaries with key value pairs
+        Returns a tablib dataset
+        :param dataset:
+        :return:
+        """
+
+        # make sure every row has the same keys.
+        # get all unique keys
+        keys = set()
+        for row in dataset:
+            keys |= set(row.keys())
+
+        # add missing keys to rows
+        for row in dataset:
+            key_set = set(keys)
+            key_set -= set(row.keys())
+            for key in key_set:
+                row[key] = None
+
+        return tablib.Dataset().load(json.dumps(dataset, sort_keys=True))
 
     @staticmethod
     def get_kobo_data(connection, dataset_id):
