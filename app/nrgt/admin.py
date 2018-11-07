8829b1e06640f334586577f9087f6f7d8315dd36
thomas-maschler <tmaschlerr@wri.org>
2018-11-07 17:22:43 -0500

    Add NRGT form and admin

--- app/nrgt/admin.py
+++ app/nrgt/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from .models import NRGTAnswer
+from .models import NRGTForm, NRGTAnswer, NRGTAnswerGS
+
+
+@admin.register(NRGTAnswerGS)
+class AnswerNRAdmin(ImportExportModelAdmin):
+    list_display = ['answer_id', 'code', 'survey_date', 'member', 'gender', 'motivation', 'instutional_framework',
+    'fairness', 'enact_decision', 'knowledge_skills', 'participation', 'transparency', 'resources', 'diversity',
+    'held_accountable', 'accountability', 'legitimacy']
+    #resource_class = NRGTAnswerGSFromFileResource
+
+
+class NRGTAnswerGSInline(admin.StackedInline):
+    model = NRGTAnswerGS
+    extra = 1
 
 
 @admin.register(NRGTAnswer)
-class NRGTAnswerAdmin(ImportExportModelAdmin):
+class AnswerAdmin(ImportExportModelAdmin):
     """
     Admin class for Connections
     """
-    pass
+    list_display = ['answer_id', 'dataset_uuid', 'landscape', 'surveyor', 'gov_group', 'women',
+                    'jurisdiction', 'members', 'survey_date', 'last_update']
+
+    inlines = [NRGTAnswerGSInline]
+    #resource_class = NRGTAnswerFromFileResource
+
+
+@admin.register(NRGTForm)
+class NRGTFormAdmin(ImportExportModelAdmin):
+    """
+    Admin class for NRGT FormsConnections
+    """
+    list_display = ['dataset_name', 'dataset_year', 'dataset_owner', 'dataset_uuid',
+                    'last_submission_time', 'last_update_time', 'last_checked_time', 'kobo_managed']
+
+
+
--- app/nrgt/models.py
+++ app/nrgt/models.py
@@ -9,9 +9,60 @@ class NRGTAnswer(models.Model):
     dataset_uuid = models.ForeignKey(KoboData, on_delete=models.CASCADE)
     landscape = models.TextField(blank=True, null=True)
     surveyor = models.TextField(blank=True, null=True)
+    gov_group = models.TextField(blank=True, null=True)
+    objective = models.TextField(blank=True, null=True)
+    women = models.FloatField(blank=True, null=True)
+    jurisdiction = models.TextField(blank=True, null=True)
+    members = models.IntegerField(blank=True, null=True)
     survey_date = models.DateTimeField(blank=True, null=True)
     last_update = models.DateTimeField(default=datetime.now, editable=False)
 
     class Meta:
         verbose_name = 'Answer'
         verbose_name_plural = 'Answers'
+
+
+class NRGTAnswerGS(models.Model):
+    answer = models.ForeignKey(NRGTAnswer, on_delete=models.CASCADE)
+    code = models.TextField(blank=True, null=True)
+    survey_date = models.DateTimeField(blank=True, null=True)
+    member = models.NullBooleanField()
+    gender = models.TextField(blank=True, null=True)
+    motivation = models.IntegerField(blank=True, null=True)
+    instutional_framework = models.IntegerField(blank=True, null=True)
+    fairness = models.IntegerField(blank=True, null=True)
+    enact_decision = models.IntegerField(blank=True, null=True)
+    knowledge_skills = models.IntegerField(blank=True, null=True)
+    participation = models.IntegerField(blank=True, null=True)
+    transparency = models.IntegerField(blank=True, null=True)
+    resources = models.IntegerField(blank=True, null=True)
+    diversity = models.IntegerField(blank=True, null=True)
+    held_accountable = models.IntegerField(blank=True, null=True)
+    accountability = models.IntegerField(blank=True, null=True)
+    legitimacy = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Answer Governance Score'
+        verbose_name_plural = 'Answers Governance Score'
+
+    def __str__(self):
+        return "{} ({})".format(self.dataset_uuid, self.answer_id)
+
+
+class NRGTForm(models.Model):
+    dataset_id = models.BigIntegerField(primary_key=True)
+    auth_user = models.ForeignKey(Connection, on_delete=models.DO_NOTHING)
+    dataset_name = models.TextField(blank=True, null=True)
+    dataset_year = models.TextField(blank=True, null=True)
+    dataset_owner = models.TextField(blank=True, null=True)
+    dataset_uuid = models.TextField(blank=True, null=True)
+    last_submission_time = models.DateTimeField(default=datetime.now, editable=False)
+    last_update_time = models.DateTimeField(default=datetime.now, editable=False)
+    last_checked_time = models.DateTimeField(default=datetime.now, editable=False)
+    kobo_managed = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'NRGT Form'
+        verbose_name_plural = 'NRGT Forms'
+        managed = False
+        db_table = 'nrgt_form'
