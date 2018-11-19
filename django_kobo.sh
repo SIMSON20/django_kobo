5dea160bebc1489f22a75cc75696a3e5dcd6e002
thomas-maschler <tmaschlerr@wri.org>
2018-11-19 17:22:31 -0500

    update init

--- django_kobo.sh
+++ django_kobo.sh
@@ -8,8 +8,8 @@ export PG_PW=$DB_PASSWORD
 case "$1" in
     init)
         type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
-        docker-compose -f docker-compose.yml build \
-        && docker-compose run web /bin/bash -c "python /home/docker/code/app/manage.py migrate && python /home/docker/code/app/manage.py collectstatic && python /home/docker/code/app/manage.py createsuperuser"
+        docker-compose -f docker-compose.yml build && docker-compose -f docker-compose.yml up \
+        && docker-compose exec web /bin/bash -c "python /home/docker/code/app/manage.py migrate && python /home/docker/code/app/manage.py collectstatic --noinput && python /home/docker/code/app/manage.py createsuperuser"
         ;;
 
     develop)
