ca6bf7be336410bb3b68a593781ed2fbe33030c9
thomas-maschler <tmaschlerr@wri.org>
2018-11-19 17:02:34 -0500

    fix nrgt import issue part 2

--- app/nrgt/resources.py
+++ app/nrgt/resources.py
@@ -21,13 +21,13 @@ class NRGTAnswerFromKoboResource(resources.ModelResource):
 
     answer_id = Field(attribute='answer_id', column_name='_uuid')
     dataset_uuid = Field(attribute='dataset_uuid', column_name='dataset_uuid')
-    landscape = Field(attribute='landscape', column_name='group_info/landscape')
-    surveyor = Field(attribute='surveyor', column_name='surveyor')
-    gov_group = Field(attribute='gov_group', column_name='group_info/gov_group')
-    objective = Field(attribute='objective', column_name='group_info/objective')
-    women = Field(attribute='women', column_name='group_info/women')
-    jurisdiction = Field(attribute='jurisdiction', column_name='group_info/jurisdiction')
-    members = Field(attribute='members', column_name='group_info/members')
+    landscape = Field(attribute='landscape', column_name='landscape')
+    surveyor = Field(attribute='surveyor', column_name='_submitted_by')
+    gov_group = Field(attribute='gov_group', column_name='gov_group')
+    objective = Field(attribute='objective', column_name='objective')
+    women = Field(attribute='women', column_name='women')
+    jurisdiction = Field(attribute='jurisdiction', column_name='jurisdiction')
+    members = Field(attribute='members', column_name='members')
     survey_date = Field(attribute='survey_date', column_name='_submission_time')
     last_update = Field(attribute='last_update', column_name='last_update')
 
@@ -37,6 +37,37 @@ class NRGTAnswerFromKoboResource(resources.ModelResource):
 
     def before_import_row(self, row, **kwargs):
         row["dataset_uuid"] = KoboData.objects.get(dataset_uuid=row["_xform_id_string"])
+
+        row["landscape"] = row["group_info/landscape"] if "group_info/landscape" in row.keys() and \
+                                                          not row["group_info/landscape"] == None else \
+            row["landscape"] if "landscape" in row.keys() else \
+                None
+
+        row["gov_group"] = row["group_info/gov_group"] if "group_info/gov_group" in row.keys() and \
+                                                          not row["group_info/gov_group"] == None else \
+            row["gov_group"] if "gov_group" in row.keys() else \
+                None
+
+        row["objective"] = row["group_info/objective"] if "group_info/objective" in row.keys() and \
+                                                          not row["group_info/objective"] == None else \
+            row["objective"] if "objective" in row.keys() else \
+                None
+
+        row["women"] = row["group_info/women"] if "group_info/women" in row.keys()  and \
+                                                         not row["group_info/women"] == None else\
+            row["women"] if "women" in row.keys() else \
+                None
+
+        row["jurisdiction"] = row["group_info/jurisdiction"] if "group_info/jurisdiction" in row.keys() and \
+                                                                not row["group_info/jurisdiction"] == None else \
+            row["jurisdiction"] if "jurisdiction" in row.keys() else \
+                None
+
+        row["members"] = row["group_info/members"] if "group_info/members" in row.keys() and \
+                                                      not row["group_info/members"] == None else \
+            row["members"] if "members" in row.keys() else \
+                None
+
         row["last_update"] = datetime.now()
 
 
