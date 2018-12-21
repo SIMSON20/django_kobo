3c4e501b43ff3eb2e2478df9a96e2aef83cad00d
thomas-maschler <tmaschlerr@wri.org>
2018-12-21 16:24:58 -0500

    restrict landscape queries

--- app/tags/templatetags/user_surveys.py
+++ app/tags/templatetags/user_surveys.py
@@ -1,4 +1,5 @@
 from django import template
+from bns.models import Answer
 
 register = template.Library()
 
@@ -12,3 +13,17 @@ def get_user_surveys(user):
 
 
 
+@register.simple_tag
+def get_user_landscapes(user):
+    if not user.is_authenticated:
+        return list()
+    else:
+        surveys = [s.dataset_uuid for s in user.kobouser.surveys.all()]
+        landscapes = Answer.objects.filter(dataset_uuid_id__in=surveys).only('landscape').order_by(
+            'landscape').distinct('landscape')
+        landscape_names = list()
+
+        for landscape in landscapes:
+            landscape_names.append(landscape.landscape)
+
+        return landscape_names
